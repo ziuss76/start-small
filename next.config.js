1e558cad19527c36fc6bae8e81517734b8c5cf5e
김지우 <ziuss76@gmail.com>
2023-12-06 23:53:20 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  scope: '/app',
+  sw: 'service-worker.js',
+});
 
-module.exports = nextConfig
+module.exports = withPWA({
+  // next.js config
+});
