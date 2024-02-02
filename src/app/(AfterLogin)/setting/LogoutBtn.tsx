c715ee43fda0fca1d932c6d65d8bf0fe0b9ce65d
김지우 <ziuss76@gmail.com>
2024-02-02 11:00:38 +0900

    Fix: 로그아웃 버튼에서 유저 이름 부분 ellipsis 추가

--- src/app/(AfterLogin)/setting/LogoutBtn.tsx
+++ src/app/(AfterLogin)/setting/LogoutBtn.tsx
@@ -16,7 +16,7 @@ export default function LogoutBtn({ userInfo }: { userInfo: UserInfo | null }) {
   return (
     <button
       type='submit'
-      className='flex w-[10rem] rounded-lg bg-slate-50 px-5 py-2.5 text-sm font-medium text-slate-900 shadow-md active:bg-slate-200 dark:bg-slate-700 dark:text-white dark:active:bg-slate-600'
+      className='flex h-[2.5rem] w-[10rem] rounded-lg bg-slate-50 px-5 py-2.5 text-sm font-medium text-slate-900 shadow-md active:bg-slate-200 dark:bg-slate-700 dark:text-white dark:active:bg-slate-600'
       onClick={() => {
         if (window.confirm('로그아웃 하시겠습니까?')) {
           signOut();
@@ -30,7 +30,12 @@ export default function LogoutBtn({ userInfo }: { userInfo: UserInfo | null }) {
         width={18}
         height={18}
       />
-      <p className='ml-1'>{userInfo?.user.name} 로그아웃</p>
+      <div className='flex items-center'>
+        <p className='ml-1 overflow-hidden text-ellipsis whitespace-nowrap'>
+          {userInfo?.user?.name}
+        </p>
+        <p className='ml-1 flex-shrink-0'>로그아웃</p>
+      </div>
     </button>
   );
 }
