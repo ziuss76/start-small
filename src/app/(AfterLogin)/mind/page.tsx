7e7a101fc518931a40f81a65301d839304a4e0a8
김지우 <ziuss76@gmail.com>
2024-03-01 00:46:44 +0900

    Fix: nav setting 길어지면 겹치는 문제

--- src/app/(AfterLogin)/mind/page.tsx
+++ src/app/(AfterLogin)/mind/page.tsx
@@ -5,7 +5,7 @@ export default function Mind() {
   return (
     <div className='flex h-full w-full justify-center'>
       <div className='mx-3 flex h-full w-full flex-col justify-start text-center'>
-        <div className='mb-3 flex h-4/6 w-full items-center justify-center rounded-lg bg-slate-300 text-center dark:bg-slate-500'>
+        <div className='mb-3 flex h-[75%] w-full items-center justify-center rounded-lg bg-slate-300 text-center dark:bg-slate-500'>
           <BreathAni />
         </div>
         <div className='flex h-1/5 w-full flex-col items-center justify-center rounded-lg bg-slate-300 text-center dark:bg-slate-500'>
