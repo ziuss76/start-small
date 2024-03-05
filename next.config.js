7504b6e6b856d468ced28b2521b6af75ccc790bf
김지우 <ziuss76@gmail.com>
2024-03-05 23:00:19 +0900

    Fix: next-pwa 수정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 
+const runtimeCaching = require('next-pwa/cache');
 const withPWA = require('next-pwa')({
   dest: 'public', // next build시 public 폴더에 sw.js를 생성
   disable: process.env.NODE_ENV === 'development', // 개발환경에서도 service worker를 사용함 (false : GenerateSW has been called multiple times 에러가 뜨지만 sw 관련 개발중일 때는 필요)
   skipWaiting: true, // sw.js가 변경되면 새로운 service worker가 활성화되기 전까지 기존의 service worker가 제어권을 유지하도록 설정
   customWorkerDir: 'public/swCustomDir',
+  runtimeCaching,
   buildExcludes: ['app-build-manifest.json'], // bad-precaching-response 에러 해결 (빌드 시 임시적으로 생성되고 삭제되는 파일)
 });
 
